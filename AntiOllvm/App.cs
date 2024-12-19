6c1c11fc9f72889bd53b7fcf9017c80529732f17
Imy <295238641@qq.com>
2024-12-19 16:48:28 +0800

    first commit

---
+++ AntiOllvm/App.cs
@@ -0,0 +1,17 @@
+﻿using AntiOllvm.Analyze;
+using AntiOllvm.Analyze.Type;
+
+namespace AntiOllvm;
+
+public class App
+{
+
+    public static void Init()
+    {
+        var readAllText = File.ReadAllText(@"E:\RiderDemo\AntiOllvm\AntiOllvm\cfg_output_0x181c6c.json");
+        Simulation simulation = new(readAllText, @"E:\RiderDemo\AntiOllvm\AntiOllvm\fix.json");
+        simulation.SetAnalyze(new CFFAnalyer());
+        simulation.Run();
+        
+    }
+}
